deede8069a065ff5376469cf1cd3524432763f66
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-10-14 14:04:39 +0100

    Start to build home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import styles from "./page.module.css"
+import { FaLongArrowAltRight } from 'react-icons/fa';
 
 export default function Home() {
-
     return (
-        <div>
-        <h1 className={styles.home_text}>Create <span className={styles.accent_color}>Unique </span>Profiles</h1>
-        </div>
-    )
+        <main>
+              <div className={styles.top}>
+                <div className={styles.centered}>
+                    <h1 className={styles.top_text}>Create <span className={styles.accentText}>Unique</span> Profiles</h1>
+                    <p className={styles.description_text}>Easy to make profiles made to enhance the developer experience!</p>
+                    
+                    <button className={styles.get_started_button}>Get Started <FaLongArrowAltRight className={styles.arrow_icon} /></button>
 
-}
+                </div>
+                </div>
+        </main>
+    )
+}   
