24aa2b22bc1959b50eb84c9efee26f282aff5210
OPTIC7409 <79322893+OPTIC7409@users.noreply.github.com>
2023-10-15 00:46:18 +0100

    Dashboard Started

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./dashboard.module.css";
+
+const Dashboard = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
