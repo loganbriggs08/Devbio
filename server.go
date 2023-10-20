751e467336c9381672bb7f8db7b9e75b079e1b6d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-10-20 23:02:53 +0100

    Setup main server.go file

--- server.go
+++ server.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"devbio/database"
+	"devbio/endpoints"
+	"log"
+	"net/http"
+
+	"github.com/pterm/pterm"
+)
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	if database.InitializeDatabase() && database.CreateTables() {
+		pterm.Success.Println("Database has been initalized successfully.")
+
+		http.HandleFunc("/api/accounts", endpoints.ManageAccounts)
+
+		log.Fatal(http.ListenAndServe(":6969", corsMiddleware(http.DefaultServeMux)))
+	} else {
+		pterm.Fatal.WithFatal(true).Println("Failed to initalize database successfully.")
+	}
+}
