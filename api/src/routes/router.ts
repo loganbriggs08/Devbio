0bfa879a90194ab5f48af18a79e3c4dfc99e4b66
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-10-20 21:33:19 +0100

    Delete old api files

--- api/src/routes/router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import bioRoutes from './bio/bio.controller';
-import userRoutes from './user/user.controller';
-
-export const router = Router();
-router.use('/bio', bioRoutes);
-router.use('/user', userRoutes);
