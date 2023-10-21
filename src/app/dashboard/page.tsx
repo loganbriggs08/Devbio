6d57d9dfe5916fdaa92b5e9013323aaa278227eb
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-10-21 16:21:42 +0100

    Check if user is logged in or not

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import styles from "./dashboard.module.css";
+'use client'
 
-const Dashboard = () => {
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
-    return (
-        <div>
-            
-        </div>
+
+const DashboardPage: React.FC = () => {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const cookies = document.cookie.split(';');
+
+    const sessionCookie = cookies.find((cookie) =>
+      cookie.trim().startsWith('session=')
     );
+
+    if (sessionCookie) {
+      fetch('http://localhost:6969/api/accounts', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          session: sessionCookie.split('=')[1],
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.username === sessionCookie.split('=')[1]) {
+            setIsLoggedIn(true);
+          } else {
+            setIsLoggedIn(false);
+            router.replace('/login');
+          }
+        })
+        .catch((error) => {
+            setIsLoggedIn(false);
+            router.replace('/login');
+        });
+    } else {
+        setIsLoggedIn(false);
+        router.replace('/login');
+    }
+  }, [router]);
+
+  return (
+    <div>
+        {isLoggedIn === true ? (
+            <div>
+                <h1>Welcome to the Dashboard</h1>
+                <p>Welcome to the dashboard.</p>
+            </div>
+        ) : null}
+    </div>
+  );
 };
 
-export default Dashboard;
+export default DashboardPage;
