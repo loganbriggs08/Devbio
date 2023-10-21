b89202b0a8c4a186918a9ca21ad23f83c74c6b4b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-10-21 11:02:40 +0100

    Send the query to the right function based on method

--- endpoints/accounts.go
+++ endpoints/accounts.go
@@ -1,9 +1,19 @@
 package endpoints
 
 import (
+	"devbio/endpoints/accounts"
+	"devbio/modules/ReturnModule"
 	"net/http"
 )
 
 func ManageAccounts(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == "POST" {
+		accounts.PostRequest(w, r)
+	} else if r.Method == "GET" {
+		accounts.GetRequest(w, r)
+	} else if r.Method == "DELETE" {
+		accounts.DeleteRequest(w, r)
+	} else {
+		ReturnModule.MethodNotAllowed(w, r)
+	}
 }
