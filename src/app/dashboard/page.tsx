4010c3d58793e709aec72b7dcfc8d0dc34a3bb7a
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-10-21 17:37:04 +0100

    Get data from API and show some basic data on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,21 @@
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
+interface UserData {
+  username: string;
+  profile_picture: string;
+  description: string;
+  skills: string[] | null;
+  interests: string[] | null;
+  location: string;
+  spoken_languages: string[] | null;
+  badges: string[];
+  is_hirable: boolean;
+  is_disabled: boolean;
+}
 
 const DashboardPage: React.FC = () => {
-  
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+  const [userData, setUserData] = useState<UserData | null>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -17,40 +28,42 @@ const DashboardPage: React.FC = () => {
     );
 
     if (sessionCookie) {
-      fetch('http://localhost:6969/api/accounts', {
+      fetch('http://localhost:6969/api/account', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           session: sessionCookie.split('=')[1],
         },
       })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.username === sessionCookie.split('=')[1]) {
-            setIsLoggedIn(true);
-          } else {
-            setIsLoggedIn(false);
-            router.replace('/login');
+        .then((response) => {
+          if (response.status === 401) {
+            throw new Error('Invalid session');
           }
+          return response.json();
+        })
+        .then((data: UserData) => {
+          setUserData(data);
         })
         .catch((error) => {
-            setIsLoggedIn(false);
-            router.replace('/login');
+          console.error(error);
+          router.replace('/login');
         });
     } else {
-        setIsLoggedIn(false);
-        router.replace('/login');
+      router.replace('/login');
     }
   }, [router]);
 
   return (
     <div>
-        {isLoggedIn === true ? (
-            <div>
-                <h1>Welcome to the Dashboard</h1>
-                <p>Welcome to the dashboard.</p>
-            </div>
-        ) : null}
+      {userData && (
+        <div>
+          <h1>Welcome to the Dashboard</h1>
+          <p>Welcome, {userData.username}!</p>
+          {userData.badges && userData.badges.length > 0 && (
+            <p>Badges: {userData.badges.join(', ')}</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
