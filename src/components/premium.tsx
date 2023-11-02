3520d89fee09b211adc0810c734f66530d525c41
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-11-02 15:10:12 +0000

    Work on implementing the premium subscriptions page

--- src/components/premium.tsx
+++ src/components/premium.tsx
@@ -1,8 +1,6 @@
-import styles from './customize.module.css';
+import styles from './premium.module.css';
 import React, { useState, useEffect } from 'react';
-import { LoadingComponent } from '@/components/loading';
 import DashboardNavbarComponent from '@/components/dashboard_navbar';
-import { BsCheckLg } from 'react-icons/bs'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { NotificationComponent } from './notification';
@@ -76,8 +74,46 @@ const PremiumComponent = () => {
         <NotificationComponent />
         <DashboardNavbarComponent userData={userData}/>
         
-        <h1>Hello Premium Page</h1>
+        <div className={styles.settings_container}>
+            <div className={styles.top_section_wrapper}>
+                <div className={styles.profile_settings_descriptor}>
+                    <h1 className={styles.profile_settings_text}>Premium Subscriptions</h1>
+                    <p className={styles.profile_description_text}>Pick a Premium Subscription and then press Upgrade.</p>
+                </div>
+            </div>
 
+            <div className={styles.divider_line}></div>
+
+            <div className={styles.premium_div_wrapper}>
+                <div className={styles.premium_div}>
+                    <div className={styles.premium_text}>
+                    <h1 className={styles.premium_text}>Premium<a className={styles.gold_text}></a>(<a className={styles.price_wrapper}>$4.99</a><a className={styles.price_wrapper}>/month</a>)</h1>
+                        <p className={styles.premium_description_text}>Unlock an extensive range of features that allow you to customize your profile precisely the way you desire.</p>
+                    </div>
+
+                    <div className={styles.purchase_button_wrapper}>
+                        <button className={styles.purchase_button}>Upgrade</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.premium_div_wrapper}>
+                <div className={styles.premium_div}>
+                    <div className={styles.premium_text}>
+                        <h1 className={styles.premium_text}>Premium<a className={styles.gold_text}>+</a>(<a className={styles.price_wrapper}>$9.99</a><a className={styles.price_wrapper}>/month</a>)</h1>
+                        <p className={styles.premium_description_text}>Unlock all features that come with <a className={styles.gold_text}>Premium</a>plus a lot more, view more advanced statistics and dive deeper into customizability.</p>
+                    </div>
+
+                    <div className={styles.purchase_button_wrapper}>
+                        <button className={styles.purchase_button}>Upgrade</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.bottom_spacing}></div>
+
+        </div>
+        
         <ToastContainer
             position="bottom-right"
             autoClose={3000}
