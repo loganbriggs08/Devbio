d2ef43757e197ec8d36548ecd5ae8c40cc9ae73b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-11-12 13:31:11 +0000

    log each request for debugging

--- server.go
+++ server.go
@@ -10,6 +10,8 @@ import (
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		pterm.Success.Println(r.Method, "Request received at", r.URL.Path)
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
